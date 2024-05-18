ca5c8f0174117b5c4775f8e00e84a7ff5016e4c8
Chayan <20je0291@cse.iitism.ac.in>
2024-05-18 11:40:50 +0530

    added gemini and prompt to check chat history

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
 from langchain_community.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
@@ -9,6 +9,26 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain_openai import ChatOpenAI
 from langchain_community.llms import HuggingFaceHub
 from htmlTemplates import css, bot_template, user_template
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+from langchain.chains.question_answering import load_qa_chain
+
+
+
+import asyncio
+
+def get_or_create_eventloop():
+    try:
+        return asyncio.get_event_loop()
+    except RuntimeError as ex:
+        if "There is no current event loop in thread" in str(ex):
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            return asyncio.get_event_loop()
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
 
 
 
@@ -21,29 +41,32 @@ def get_pdf_text(pdf_docs):
     return text
     
 def get_text_chunks(raw_text):
-    text_splitter = CharacterTextSplitter(
-        separator = "\n",
+    text_splitter = RecursiveCharacterTextSplitter(
         chunk_size = 1000,
-        chunk_overlap = 200,
-        length_function = len
+        chunk_overlap = 1000,
     )
     chunks = text_splitter.split_text(raw_text)
     return chunks
     
 def get_vector_store(text_chunks):
     # embeddings = OpenAIEmbeddings()
-    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     vector_store = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-    return vector_store
+    vector_store.save_local("faiss_index")
+
     
-def get_conversation_chain(vector_store):
+def get_conversation_chain():
     # llm = ChatOpenAI()
-    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-    
+    llm = HuggingFaceHub(repo_id="google/flan-t5-base", model_kwargs={"temperature":0.5, "max_length":512})
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+
     memory = ConversationBufferMemory(
         memory_key='chat_history',
         return_messages=True
     )
+    vector_store = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm = llm,
         retriever = vector_store.as_retriever(),
@@ -51,17 +74,58 @@ def get_conversation_chain(vector_store):
     )
     return conversation_chain
 
+
+
+def get_conversation_chain_gemini():
+
+    prompt_template = """
+    The foloowing question can be a standalone question or a follow up question based on the chat history.
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Chat History:\n{chat_history}\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+
+    
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context","chat_history", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain
+
+
+
 def handle_user_input(user_input):
-    response = st.session_state.conversation({'question':user_input})
-    st.session_state.chat_history = response['chat_history']
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    docs = new_db.similarity_search(user_input)
+
+    chain = get_conversation_chain_gemini()
+    st.session_state.conversation = chain
+
+    
+    response = chain(
+        {"input_documents":docs, "chat_history":st.session_state.chat_history, "question": user_input}
+        , return_only_outputs=False
+        )
+    
+    # print(response)
+    st.write("Reply: ", response["output_text"])
+    
+    st.session_state.chat_history.append({'question':user_input,'answer':response['output_text']})
+    # print(st.session_state.chat_history)
     
     for i, message in enumerate(st.session_state.chat_history):
-        if  i%2 == 0:
-            st.write(user_template.replace("{{MSG}}",message.content),unsafe_allow_html=True)
-        else:
-            
-            st.write(bot_template.replace("{{MSG}}",message.content),unsafe_allow_html=True)
+        # st.write(message['question'],unsafe_allow_html=True)
+        st.write(user_template.replace("{{MSG}}",message['question']),unsafe_allow_html=True)
+        st.write(bot_template.replace("{{MSG}}",message['answer']),unsafe_allow_html=True)
     
+
 def main():
     load_dotenv()
     st.set_page_config(page_title="Q&A on PDFs",page_icon=":books:")
@@ -70,7 +134,7 @@ def main():
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
     if "chat_history" not in st.session_state:
-        st.session_state.chat_history = None
+        st.session_state.chat_history = []
         
     st.header("Q&A on PDFs :books:")
     user_question = st.text_input("Ask a question about your documents:")
@@ -88,17 +152,10 @@ def main():
         pdf_docs = st.file_uploader("Upload ypur PDFs here and click on 'Process'", accept_multiple_files=True)
         if st.button("Process"):
             with st.spinner("Processing..."):
-                # get pdf text
                 raw_text = get_pdf_text(pdf_docs)
-                
-                # get chunks
                 text_chunks = get_text_chunks(raw_text)
-                
-                # create vector store
-                vector_store = get_vector_store(text_chunks)
-                
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(vector_store)
+                get_vector_store(text_chunks)
+                st.success("Done!")
     
 
 if __name__ == '__main__':
